05cc6b25522646f372f76467644db1439dfc0dbc
Nicolas Parada <nicolas.pc.1700@gmail.com>
2022-09-14 19:24:38 -0500

    se agrega y aprende @input y @output junto a EventEmitter

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
+
 export class ImgComponent implements OnInit {
+
+  @Input() img: string = '';
+  @Output() loaded = new EventEmitter<string>();
+  imgDefault = './assets/imagenes/default.png';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  imgError(){
+    this.img = this.imgDefault;
+  }
+
+  imgLoad(){
+    console.log('Log Hijo');
+    this.loaded.emit(this.img);
   }
 
 }
